9b1e7b9552287e7d41e5aebf9ad332a229072365
rachelclaire <RachelPro@Rachels-MacBook-Pro.local>
2015-11-23 12:06:28 -0500

    fixed loop crap

--- youtube/main.js
+++ youtube/main.js
@@ -1,3 +1,28 @@
-var videoId = "4IP_E7efGWE";
+/*var videoId = "4IP_E7efGWE";
 $iframe = $("<iframe>")
-$iframe.src("https://www.youtube.com/embed/" + videoId);
+$iframe.src("https://www.youtube.com/embed/" + videoId);*/
+
+function addBoxes(data) {
+	for(var i = 0; i < data.items.length; i++) {
+    addThumbnail(data.items[i]);
+}
+	console.log(data)
+  
+}
+
+
+function addThumbnail(video){
+
+var imageUrl = video.snippet.thumbnails.medium.url	
+	
+var $container = $(".container");
+var $box = $("<div class='box'>");
+var $img = $("<img>").attr("src",imageUrl);
+
+$box.append($img);
+$container.append($box);
+}
+
+
+/*addItems
+var thumbnail = .item(0).snippet.thumbnails.medium.url*/
